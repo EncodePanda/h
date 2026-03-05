cc7f4d3b559c086cdc244e097c0e2e86dd11bc3f
EncodePanda <paul.szulc@gmail.com>
2026-03-05 11:06:28 +0100

    Setup locales

--- home.nix
+++ home.nix
@@ -27,6 +27,9 @@
     # . "~/.cargo/env"
     initContent = ''
       . ~/.nix-profile/etc/profile.d/nix.sh
+      export LANG=en_US.UTF-8
+      export LANGUAGE=en_US.UTF-8
+      export LC_ALL=en_US.UTF-8
       export NVM_DIR="$HOME/.nvm"
       [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
       [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
